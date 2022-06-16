60a13ba731fd8b1c08112bc13f2a6e93394ebf90
kaio <kaiocardoso_1995@hotmail.com>
2022-06-16 14:16:11 -0300

    Criado tela de listagem de eventos

--- core/admin.py
+++ core/admin.py
@@ -5,6 +5,6 @@ from core.models import Evento
 
 class EventoAdmin(admin.ModelAdmin):
     list_display = ('titulo', 'data_evento', 'data_criacao')
-    list_filter = ('titulo', 'usuario', 'data-evento',)
+
 
 admin.site.register(Evento, EventoAdmin)
